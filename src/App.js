a70de2792f7f2167fe350ab2baf5624c297ca1a2
ahmedBazzara <ahmed1995bazzara@gmail.com>
2023-11-05 19:45:45 +0100

    Edit card

--- src/App.js
+++ src/App.js
@@ -1,64 +1,92 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import AddCard from "./components/AddCard";
 import { data as testData } from "./test";
 import Card from "./components/Card";
 
 function App() {
-  const [data, setData] = useState(testData);
-  
-  const [img, setimg] = useState("");
-  const [title, settitle] = useState("");
-  const [text, settext] = useState("");
+    const [data, setData] = useState(testData);
 
-  function deleteCard(id) {
-    setData((prev) => prev.filter((item) => item.id !== id));
-  }
+    const [img, setimg] = useState("");
+    const [title, settitle] = useState("");
+    const [text, settext] = useState("");
 
-  function addCard() {
-    const newCard = {
-      id: uuidv4(),
-      title: title,
-      description: text,
-      image: img
+    const [editCardId, setEditCardId] = useState("");
+
+    function deleteCard(id) {
+        setData((prev) => prev.filter((item) => item.id !== id));
     }
 
-    setData(prev => {
-      return [...prev, newCard]
-    })
+    function addCard() {
+        const newCard = {
+            id: uuidv4(),
+            title: title,
+            description: text,
+            image: img,
+        };
 
-    setimg("")
-    settitle("")
-    settext("")
-  }
+        setData((prev) => {
+            return [...prev, newCard];
+        });
 
-  return (
-    <div className="App">
-      <AddCard
-        img={img}
-        setimg={setimg}
-        title={title}
-        settitle={settitle}
-        text={text}
-        settext={settext}
-        onSubmit={addCard}
-      />
+        setimg("");
+        settitle("");
+        settext("");
+    }
 
-      <div className="card">
-        {data.map((el) => (
-          <Card
-            id={el.id}
-            key={el.id}
-            image={el.image}
-            title={el.title}
-            description={el.description}
-            deleteCard={() => deleteCard(el.id)}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    function handleSetEditCardId(id) {
+        setEditCardId(id);
+    }
+
+    function confirmEdit(newCardData) {
+        console.log("confirmed", newCardData);
+
+        setData((prev) => {
+            return prev.map((el) => {
+                if (el.id !== editCardId) return el;
+
+                return {
+                    ...el,
+                    title: newCardData.title,
+                    description: newCardData.description,
+                };
+                // return {...el, ...newCardData}
+            });
+        });
+
+        setEditCardId("");
+    }
+
+    return (
+        <div className="App">
+            <AddCard
+                img={img}
+                setimg={setimg}
+                title={title}
+                settitle={settitle}
+                text={text}
+                settext={settext}
+                onSubmit={addCard}
+            />
+
+            <div className="card">
+                {data.map((el) => (
+                    <Card
+                        id={el.id}
+                        key={el.id}
+                        image={el.image}
+                        title={el.title}
+                        description={el.description}
+                        deleteCard={() => deleteCard(el.id)}
+                        setEditCardId={handleSetEditCardId}
+                        isEditing={el.id === editCardId}
+                        confirmEdit={confirmEdit}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
