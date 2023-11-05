91abd9ba3ad567cc3defd58de462467e62d3e040
ahmedBazzara <ahmed1995bazzara@gmail.com>
2023-11-05 18:04:12 +0100

    Move state to App(). fix adding new card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 import "./App.css";
 import AddCard from "./components/AddCard";
 import { data as testData } from "./test";
@@ -6,13 +7,44 @@ import Card from "./components/Card";
 
 function App() {
   const [data, setData] = useState(testData);
+  
+  const [img, setimg] = useState("");
+  const [title, settitle] = useState("");
+  const [text, settext] = useState("");
+
   function deleteCard(id) {
     setData((prev) => prev.filter((item) => item.id !== id));
   }
 
+  function addCard() {
+    const newCard = {
+      id: uuidv4(),
+      title: title,
+      description: text,
+      image: img
+    }
+
+    setData(prev => {
+      return [...prev, newCard]
+    })
+
+    setimg("")
+    settitle("")
+    settext("")
+  }
+
   return (
     <div className="App">
-      <AddCard />
+      <AddCard
+        img={img}
+        setimg={setimg}
+        title={title}
+        settitle={settitle}
+        text={text}
+        settext={settext}
+        onSubmit={addCard}
+      />
+
       <div className="card">
         {data.map((el) => (
           <Card
