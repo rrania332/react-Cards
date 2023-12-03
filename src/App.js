1b626947605cc226bfd2bfce6c021cfddb40f052
rrania332 <raniaalkazem@gmail.com>
2023-12-03 21:16:39 +0300

    form_design

--- src/App.js
+++ src/App.js
@@ -6,87 +6,86 @@ import { data as testData } from "./test";
 import Card from "./components/Card";
 
 function App() {
-    const [data, setData] = useState(testData);
+  const [data, setData] = useState(testData);
+  const [img, setimg] = useState("");
+  const [title, settitle] = useState("");
+  const [text, settext] = useState("");
 
-    const [img, setimg] = useState("");
-    const [title, settitle] = useState("");
-    const [text, settext] = useState("");
+  const [editCardId, setEditCardId] = useState("");
 
-    const [editCardId, setEditCardId] = useState("");
+  function deleteCard(id) {
+    setData((prev) => prev.filter((item) => item.id !== id));
+  }
 
-    function deleteCard(id) {
-        setData((prev) => prev.filter((item) => item.id !== id));
-    }
+  function addCard() {
+    const newCard = {
+      id: uuidv4(),
+      title: title,
+      description: text,
+      image: img,
+    };
 
-    function addCard() {
-        const newCard = {
-            id: uuidv4(),
-            title: title,
-            description: text,
-            image: img,
+    setData((prev) => {
+      return [...prev, newCard];
+    });
+
+    setimg("");
+    settitle("");
+    settext("");
+  }
+
+  function handleSetEditCardId(id) {
+    setEditCardId(id);
+  }
+
+  function confirmEdit(newCardData) {
+    console.log("confirmed", newCardData);
+
+    setData((prev) => {
+      return prev.map((el) => {
+        if (el.id !== editCardId) return el;
+
+        return {
+          ...el,
+          title: newCardData.title,
+          description: newCardData.description,
         };
+        // return {...el, ...newCardData}
+      });
+    });
 
-        setData((prev) => {
-            return [...prev, newCard];
-        });
+    setEditCardId("");
+  }
 
-        setimg("");
-        settitle("");
-        settext("");
-    }
+  return (
+    <div className="App">
+      <AddCard
+        img={img}
+        setimg={setimg}
+        title={title}
+        settitle={settitle}
+        text={text}
+        settext={settext}
+        onSubmit={addCard}
+      />
 
-    function handleSetEditCardId(id) {
-        setEditCardId(id);
-    }
-
-    function confirmEdit(newCardData) {
-        console.log("confirmed", newCardData);
-
-        setData((prev) => {
-            return prev.map((el) => {
-                if (el.id !== editCardId) return el;
-
-                return {
-                    ...el,
-                    title: newCardData.title,
-                    description: newCardData.description,
-                };
-                // return {...el, ...newCardData}
-            });
-        });
-
-        setEditCardId("");
-    }
-
-    return (
-        <div className="App">
-            <AddCard
-                img={img}
-                setimg={setimg}
-                title={title}
-                settitle={settitle}
-                text={text}
-                settext={settext}
-                onSubmit={addCard}
-            />
-
-            <div className="card">
-                {data.map((el) => (
-                    <Card
-                        id={el.id}
-                        key={el.id}
-                        image={el.image}
-                        title={el.title}
-                        description={el.description}
-                        deleteCard={() => deleteCard(el.id)}
-                        setEditCardId={handleSetEditCardId}
-                        isEditing={el.id === editCardId}
-                        confirmEdit={confirmEdit}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+      <div className="card">
+        {data.map((el) => (
+          <Card
+            id={el.id}
+            key={el.id}
+            image={el.image}
+            title={el.title}
+            description={el.description}
+            deleteCard={() => deleteCard(el.id)}
+            setEditCardId={handleSetEditCardId}
+            isEditing={el.id === editCardId}
+            confirmEdit={confirmEdit}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
